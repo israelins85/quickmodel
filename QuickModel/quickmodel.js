0a6969c2c0a3e026231fdcc8ef3e4a78ffb06f2f
Israel Lins Albuquerque <i.lins85@icloud.com>
2024-05-31 22:16:10 +0200

    Revert "join feature;"
    This reverts commit bf2e84a4f7bd2059dae968f3f6822aae9fc8b756.

--- QuickModel/quickmodel.js
+++ QuickModel/quickmodel.js
@@ -564,20 +564,6 @@ QMModel.prototype = {
         this.filterConditions = conditions
         return this.get()
     },
-    "join": function (table, onFilter, type = "JOIN") {
-        if (!this.joins) {
-            this.joins = []
-        }
-        this.joins.push({
-                            "type": type,
-                            "table": table,
-                            "onFilter": onFilter
-                        })
-        return this
-    },
-    "leftJoin": function (table, onFilter) {
-        return this.join(table, onFilter, "LEFT JOIN")
-    },
     "order": function (sorters) {
         if (typeof sorters === 'string') {
             if (!this.sorters) {
@@ -601,15 +587,7 @@ QMModel.prototype = {
         return null
     },
     "all": function () {
-        var sql = `SELECT * FROM ${this._meta.tableName}`
-
-        if (this.joins) {
-            for (var idxJoin = 0; idxJoin < this.joins.length; idxJoin++) {
-                const jDef = this.joins[idxJoin]
-                sql += ` ${jDef["type"]} ${jDef["table"]} ON (${jDef["onFilter"]}) `
-            }
-        }
-
+        var sql = "SELECT * FROM " + this._meta.tableName
         sql += this._defineWhereClause()
 
         if (this.sorters && this.sorters.constructor === String) {
